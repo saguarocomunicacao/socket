cce5516bb94e4d663a025be6b672bb14c8f8bc56
SAGUARO Comunicação <84872772+saguarocomunicacao@users.noreply.github.com>
2024-09-04 17:33:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ server.listen(port, () => {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   next();
 });
 
