ae250bb2309cf270dee19e27512542f10ce4200c
SAGUARO Comunicação <84872772+saguarocomunicacao@users.noreply.github.com>
2024-09-05 16:47:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+// app.js
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-// Configuração do socket.io com CORS e transporte forçado para WebSocket
+//var io = require('socket.io')(server);
 var io = require('socket.io')(server, {
   cors: {
     origin: "*",  // ou especifique o domínio correto
     methods: ["GET", "POST"]
-  },
-  transports: ['websocket']  // Força o uso de WebSocket
+  }
 });
 
 app.use(express.static(__dirname + '/node_modules'));
@@ -20,31 +20,22 @@ server.listen(port, () => {
   console.log(__dirname + '/node_modules');
 });
 
-// Middleware para configurar CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// Rota principal
-app.get('/', function(req, res, next) {  
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Gerenciando conexões do socket.io
 io.on('connection', function(client) {
-  console.log('Client connected:', client.id);
+    console.log('Client connected...');
 
-  // Evento de recebimento de mensagens
-  client.on('messages', function(data) {
-    console.log(`${data.broad} -> ${data.user}: ${data.msg}`);
-    client.emit(data.broad, data);
-    client.broadcast.emit(data.broad, data);
-  });
-
-  // Evento de desconexão
-  client.on('disconnect', (reason) => {
-    console.log(`Client ${client.id} disconnected: ${reason}`);
-  });
+	client.on('messages', function(data) {
+		console.log(""+data.broad+" -> "+data.user+": "+data.msg);
+		client.emit(""+data.broad+"", data);
+		client.broadcast.emit(""+data.broad+"",data);
+	});
 });
